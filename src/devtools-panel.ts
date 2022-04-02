1bec7314f53a79c3636fb2fa504f90dda044890d
Vildan Softic <zewa666@gmail.com>
2022-04-02 17:21:56 +0200

    feat: teleport; stop; moveRandomly
    https://github.com/zewa666/grid-engine-devtools/issues/3

--- src/devtools-panel.ts
+++ src/devtools-panel.ts
@@ -8,15 +8,30 @@ export class DevtoolsPanel {
   public characters: string[] = [];
   @observable() public selectedCharacter: string;
   public moveToInput: string;
+  public moveRandomDelayInput: number;
+  public moveRandomRadiusInput: number;
 
   async attaching() {
     this.msg = this.isGridEngineAvailable() ? "Grid Engine connected" : "No Grid Engine found";
     this.characters = await evaluate<string[]>(`window.${SUT}.getAllCharacters()`);
   }
 
+  public async stopMovement() {
+    await evaluate(`window.${SUT}.stopMovement("${this.selectedCharacter}")`);
+  }
+
+  public async teleport() {
+    const [x, y] = this.moveToInput.split(",");
+    await evaluate(`window.${SUT}.setPosition("${this.selectedCharacter}", { x: ${x}, y: ${y}})`);
+  }
+
   public async moveTo() {
     const [x, y] = this.moveToInput.split(",");
     await evaluate(`window.${SUT}.moveTo("${this.selectedCharacter}", { x: ${x}, y: ${y}})`);
+  }
+
+  public async moveRandom() {
+    await evaluate(`window.${SUT}.moveRandomly("${this.selectedCharacter}", ${this.moveRandomDelayInput || 0}, ${this.moveRandomRadiusInput || -1})`);
   }
 
   private async selectedCharacterChanged() {
